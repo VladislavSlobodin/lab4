2a92172260d1691305008de44c6c500eb6efbd84
vladi <vladi@DESKTOP-QGDSMGT>
2023-10-15 20:33:37 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            WordDataGridView = new DataGridView();
+            tabControl1 = new TabControl();
+            WordTab = new TabPage();
+            menuStrip1 = new MenuStrip();
+            открытьШаблонToolStripMenuItem = new ToolStripMenuItem();
+            сохранитьРезультатToolStripMenuItem = new ToolStripMenuItem();
+            ExcelTab = new TabPage();
+            ExcelDataGridView = new DataGridView();
+            menuStrip2 = new MenuStrip();
+            рассчитатьToolStripMenuItem = new ToolStripMenuItem();
+            выбратьПутьДляСохраненияToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)WordDataGridView).BeginInit();
+            tabControl1.SuspendLayout();
+            WordTab.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            ExcelTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ExcelDataGridView).BeginInit();
+            menuStrip2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // WordDataGridView
+            // 
+            WordDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            WordDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            WordDataGridView.Dock = DockStyle.Fill;
+            WordDataGridView.Location = new Point(3, 3);
+            WordDataGridView.Name = "WordDataGridView";
+            WordDataGridView.RowTemplate.Height = 25;
+            WordDataGridView.Size = new Size(786, 416);
+            WordDataGridView.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(WordTab);
+            tabControl1.Controls.Add(ExcelTab);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 3;
+            // 
+            // WordTab
+            // 
+            WordTab.Controls.Add(menuStrip1);
+            WordTab.Controls.Add(WordDataGridView);
+            WordTab.Location = new Point(4, 24);
+            WordTab.Name = "WordTab";
+            WordTab.Padding = new Padding(3);
+            WordTab.Size = new Size(792, 422);
+            WordTab.TabIndex = 0;
+            WordTab.Text = "Word";
+            WordTab.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { открытьШаблонToolStripMenuItem, сохранитьРезультатToolStripMenuItem });
+            menuStrip1.Location = new Point(3, 3);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(786, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьШаблонToolStripMenuItem
+            // 
+            открытьШаблонToolStripMenuItem.Name = "открытьШаблонToolStripMenuItem";
+            открытьШаблонToolStripMenuItem.Size = new Size(114, 20);
+            открытьШаблонToolStripMenuItem.Text = "Открыть шаблон";
+            // 
+            // сохранитьРезультатToolStripMenuItem
+            // 
+            сохранитьРезультатToolStripMenuItem.Name = "сохранитьРезультатToolStripMenuItem";
+            сохранитьРезультатToolStripMenuItem.Size = new Size(134, 20);
+            сохранитьРезультатToolStripMenuItem.Text = "Сохранить результат";
+            // 
+            // ExcelTab
+            // 
+            ExcelTab.Controls.Add(ExcelDataGridView);
+            ExcelTab.Controls.Add(menuStrip2);
+            ExcelTab.Location = new Point(4, 24);
+            ExcelTab.Name = "ExcelTab";
+            ExcelTab.Padding = new Padding(3);
+            ExcelTab.Size = new Size(792, 422);
+            ExcelTab.TabIndex = 1;
+            ExcelTab.Text = "Excel";
+            ExcelTab.UseVisualStyleBackColor = true;
+            // 
+            // ExcelDataGridView
+            // 
+            ExcelDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ExcelDataGridView.Dock = DockStyle.Fill;
+            ExcelDataGridView.Location = new Point(3, 27);
+            ExcelDataGridView.Name = "ExcelDataGridView";
+            ExcelDataGridView.RowTemplate.Height = 25;
+            ExcelDataGridView.Size = new Size(786, 392);
+            ExcelDataGridView.TabIndex = 0;
+            // 
+            // menuStrip2
+            // 
+            menuStrip2.Items.AddRange(new ToolStripItem[] { выбратьПутьДляСохраненияToolStripMenuItem, рассчитатьToolStripMenuItem });
+            menuStrip2.Location = new Point(3, 3);
+            menuStrip2.Name = "menuStrip2";
+            menuStrip2.Size = new Size(786, 24);
+            menuStrip2.TabIndex = 1;
+            menuStrip2.Text = "menuStrip2";
+            // 
+            // рассчитатьToolStripMenuItem
+            // 
+            рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+            рассчитатьToolStripMenuItem.Size = new Size(80, 20);
+            рассчитатьToolStripMenuItem.Text = "Рассчитать";
+            рассчитатьToolStripMenuItem.Click += рассчитатьToolStripMenuItem_Click;
+            // 
+            // выбратьПутьДляСохраненияToolStripMenuItem
+            // 
+            выбратьПутьДляСохраненияToolStripMenuItem.Name = "выбратьПутьДляСохраненияToolStripMenuItem";
+            выбратьПутьДляСохраненияToolStripMenuItem.Size = new Size(183, 20);
+            выбратьПутьДляСохраненияToolStripMenuItem.Text = "Выбрать путь для сохранения";
+            выбратьПутьДляСохраненияToolStripMenuItem.Click += выбратьПутьДляСохраненияToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)WordDataGridView).EndInit();
+            tabControl1.ResumeLayout(false);
+            WordTab.ResumeLayout(false);
+            WordTab.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ExcelTab.ResumeLayout(false);
+            ExcelTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ExcelDataGridView).EndInit();
+            menuStrip2.ResumeLayout(false);
+            menuStrip2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private DataGridView WordDataGridView;
+        private TabControl tabControl1;
+        private TabPage WordTab;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem открытьШаблонToolStripMenuItem;
+        private ToolStripMenuItem сохранитьРезультатToolStripMenuItem;
+        private TabPage ExcelTab;
+        private DataGridView ExcelDataGridView;
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem рассчитатьToolStripMenuItem;
+        private ToolStripMenuItem выбратьПутьДляСохраненияToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,327 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using SharpDX.Direct2D1;
+using Border = DocumentFormat.OpenXml.Spreadsheet.Border;
+using BottomBorder = DocumentFormat.OpenXml.Spreadsheet.BottomBorder;
+using Run = DocumentFormat.OpenXml.Wordprocessing.Run;
+using Text = DocumentFormat.OpenXml.Wordprocessing.Text;
+
+namespace lab4;
+
+public partial class Form1 : Form
+{
+    #region WordVariables
+    private List<string> _wordLabels = new()
+    {
+        "Имя_Отчество",
+        "Событие",
+        "Дата",
+        "Адрес",
+        "Имя_Отчество_2",
+    };
+    private Dictionary<string, string> _keyValuePairs = new();
+    private string _examplePath = string.Empty;
+    private static readonly string _filter = "Word files (*.docx)|*.docx|All files (*.*)|*.*";
+    #endregion
+    #region ExcelVariables
+    private List<string> _excelLabels = new()
+    {
+        "Регион",
+        "Лыжи",
+        "Коньки",
+        "Сани",
+        "Всего",
+    };
+    private string _excelDocumentPath = string.Empty;
+    #endregion
+
+    public Form1()
+    {
+        InitializeComponent();
+        InitializeWordTable();
+        InitializeExcelTable();
+    }
+
+    #region Word
+    private void InitializeWordTable()
+    {
+        WordDataGridView.Columns.AddRange(new DataGridViewTextBoxColumn[] { new() { HeaderText = "Шаблон", Name = "ExampleColumn" }, new() { HeaderText = "Значение", Name = "ValueColumn" } });
+        _wordLabels.ForEach(l => WordDataGridView.Rows.Add(l, string.Empty));
+    }
+
+    private void ParseWordTable() =>
+        WordDataGridView.Rows
+                     .Cast<DataGridViewRow>()
+                     .Take(WordDataGridView.Rows.Count - 1)
+                     .ToList()
+                     .ForEach(r => _keyValuePairs.Add(r.Cells[0].Value.ToString()!.Replace(" ", "_"), r.Cells[1].Value.ToString() ?? string.Empty));
+
+    private void CreateWordDocument(string examplePath, string savePath)
+    {
+        using WordprocessingDocument document = WordprocessingDocument.CreateFromTemplate(examplePath);
+        var body = document.MainDocumentPart!.Document.Body!;
+        var keys = _keyValuePairs.Keys.ToList();
+        foreach (Paragraph paragraph in body.Elements<Paragraph>())
+        {
+            foreach (Run run in paragraph.Elements<Run>())
+            {
+                foreach (Text text in run.Elements<Text>())
+                {
+                    for (var i = 0; i < keys.Count; i++)
+                    {
+                        if (text.Text.Contains(keys[i]))
+                        {
+                            text.Text = text.Text.Replace(keys[i], _keyValuePairs[keys[i]]);
+                            keys.Remove(keys[i--]);
+                        }
+                    }
+                }
+            }
+        }
+
+        document.Clone(savePath);
+    }
+
+    private void открытьШаблонToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        OpenFileDialog openFileDialog = new()
+        {
+            Filter = _filter
+        };
+
+        if (openFileDialog.ShowDialog() != DialogResult.OK)
+        {
+            MessageBox.Show("Не выбран шаблон", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        _examplePath = openFileDialog.FileName;
+    }
+
+    private void сохранитьРезультатToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        if (_examplePath == string.Empty)
+        {
+            MessageBox.Show("Не выбран шаблон", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        ParseWordTable();
+        SaveFileDialog saveFileDialog = new()
+        {
+            Filter = _filter,
+            FileName = "Result.docx",
+        };
+
+        if (saveFileDialog.ShowDialog() != DialogResult.OK)
+        {
+            MessageBox.Show("Не выбран путь для сохранения", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        CreateWordDocument(_examplePath, saveFileDialog.FileName);
+    }
+    #endregion
+    #region Excel
+    private void InitializeExcelTable() => _excelLabels
+        .Take(_excelLabels.Count - 1)
+        .ToList()
+        .ForEach(l => ExcelDataGridView.Columns.Add(l, l));
+
+    private bool TryParseExcelTable(out object[][] model)
+    {
+        model = ExcelDataGridView
+            .Rows
+            .Cast<DataGridViewRow>()
+            .Take(ExcelDataGridView.Rows.Count - 1)
+            .Select(r => TryParseRow(r, out var objects) ? objects : null)
+            .ToArray()!;
+        return !model.Any(x => x is null);
+    }
+
+    private bool TryParseRow(DataGridViewRow row, out object[] objects)
+    {
+        var cells = row.Cells;
+        List<object> result = new()
+        {
+            row.Cells[0].Value.ToString() ?? string.Empty
+        };
+
+        for (int i = 1; i < cells.Count; i++)
+        {
+            if (!int.TryParse(cells[i].Value?.ToString() ?? string.Empty, out int value))
+            {
+                objects = Array.Empty<object>();
+                return false;
+            }
+
+            result.Add(value);
+        }
+
+        objects = result.ToArray();
+        return true;
+    }
+
+    private void выбратьПутьДляСохраненияToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        SaveFileDialog saveFileDialog = new()
+        {
+            Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*",
+            FileName = "Result",
+        };
+
+        if (saveFileDialog.ShowDialog() != DialogResult.OK)
+        {
+            MessageBox.Show("Не выбран путь для сохранения", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        _excelDocumentPath = saveFileDialog.FileName;
+    }
+
+    private void рассчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        if (!TryParseExcelTable(out var model))
+        {
+            MessageBox.Show("Таблица заполнена некорректно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        CreateExcelDocument(_excelDocumentPath, model);
+        MessageBox.Show("Документ создан", "Успех", MessageBoxButtons.OK, MessageBoxIcon.None);
+    }
+
+    private void CreateExcelDocument(string path, object[][] model)
+    {
+        using var document = SpreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook);
+        WorkbookPart workbookPart = document.AddWorkbookPart();
+        workbookPart.Workbook = new Workbook();
+        WorkbookStylesPart stylesPart = workbookPart.AddNewPart<WorkbookStylesPart>();
+        stylesPart.Stylesheet = new Stylesheet();
+        WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+        SheetData sheetData = new SheetData();
+        worksheetPart.Worksheet = new Worksheet(sheetData);
+        Sheets sheets = document.WorkbookPart!.Workbook.AppendChild(new Sheets());
+        Sheet sheet = new()
+        {
+            Id = document.WorkbookPart.GetIdOfPart(worksheetPart),
+            SheetId = 1,
+            Name = "Расчеты",
+        };
+
+        sheetData.Append(CreateHeader());
+        for (int i = 0; i < model.Length; i++)
+        {
+            var row = CreateRow(model[i], i);
+            sheetData.Append(row);
+        }
+
+        sheetData.Append(CreateFooter(model.Length + 2));
+        sheets.Append(sheet);
+        workbookPart.Workbook.Save();
+    }
+
+    private Row CreateHeader()
+    {
+        Row header = new() { RowIndex = 1 };
+        for (int j = 0; j < _excelLabels.Count; j++)
+        {
+            header.Append(new Cell()
+            {
+                CellReference = GetCellReference(0, j),
+                CellValue = new(_excelLabels[j])
+            });
+        }
+
+        return header;
+    }
+
+    private Row CreateFooter(int rowIndex)
+    {
+        Row footer = new() { RowIndex = Convert.ToUInt32(rowIndex) };
+        footer.Append(new Cell()
+        {
+            CellReference = GetCellReference(rowIndex - 1, 0),
+            CellValue = new("Среднее")
+        });
+
+        for (int j = 1; j < _excelLabels.Count; j++)
+        {
+            footer.Append(new Cell()
+            {
+                CellReference = GetCellReference(rowIndex - 1, j),
+                CellFormula = new($"=AVERAGE({GetCellReference(1, j)}:{GetCellReference(rowIndex - 2, j)})"),
+            });
+        }
+
+        return footer;
+    }
+
+    private static Row CreateRow(object[] model, int index)
+    {
+        Row row = new() { RowIndex = Convert.ToUInt32(index + 2) };
+        for (int j = 0; j < model.Length; j++)
+        {
+            Cell cell = new()
+            {
+                CellReference = GetCellReference(index + 1, j),
+                CellValue = new(model[j].ToString()!),
+            };
+
+            row.Append(cell);
+        }
+
+        Cell formulaCell = new()
+        {
+            CellReference = GetCellReference(index + 1, model.Length),
+            CellFormula = new($"=SUM({GetCellReference(index + 1, 1)}:{GetCellReference(index + 1, model.Length - 1)})"),
+        };
+
+        row.Append(formulaCell);
+        return row;
+    }
+
+    private static string GetCellReference(int i, int j) => $"{(char)('A' + j)}{i + 1}";
+    #endregion
+}
+
+/*
+ public void CreateStylesheet(string filePath)
+ {
+     using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook))
+     {
+         WorkbookPart workbookPart = spreadsheetDocument.AddWorkbookPart();
+         workbookPart.Workbook = new Workbook();
+ 
+         WorkbookStylesPart stylesPart = workbookPart.AddNewPart<WorkbookStylesPart>();
+         stylesPart.Stylesheet = new Stylesheet();
+ 
+         // Add cell formats
+         stylesPart.Stylesheet.CellFormats = new CellFormats();
+         stylesPart.Stylesheet.CellFormats.AppendChild(new CellFormat());
+ 
+         // Add fonts
+         stylesPart.Stylesheet.Fonts = new Fonts();
+         stylesPart.Stylesheet.Fonts.AppendChild(new Font());
+ 
+         // Add fills
+         stylesPart.Stylesheet.Fills = new Fills();
+         stylesPart.Stylesheet.Fills.AppendChild(new Fill());
+ 
+         // Add borders
+         stylesPart.Stylesheet.Borders = new Borders();
+         stylesPart.Stylesheet.Borders.AppendChild(new Border());
+ 
+         stylesPart.Stylesheet.Save();
+     }
+ }
+ 
+ 
+ Stylesheet stylesheet = workbookPart.WorkbookStylesPart!.Stylesheet;
+ CellFormats cellformats = stylesheet.CellFormats;
+ Border border = new();
+ border.BottomBorder = new BottomBorder() { Style = BorderStyleValues.Thick };
+ cellformats.AppendChild(border);
+ */
